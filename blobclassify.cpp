129dd265d0df7eb095bb2bf893521d1f9ec90f8b
Camille Maurice E324821nbsp <e324821@6B-11-67-60.(none)>
2015-04-23 15:40:07 +0200

    classification

--- blobclassify.cpp
+++ blobclassify.cpp
@@ -110,13 +110,13 @@ IplImage *paintBlobClasses(IplImage* frame, BlobList *pBlobList)
 		y = ith_blob->getY();
 		
 		if(ith_blob->getlabel() == CAR){
-			cvRectangle( blobImage, cvPoint(x,y), cvPoint(W+x, H+x), CV_RGB( 100, 0, 0 ), 2, 8, 0 );
+			cvRectangle( blobImage, cvPoint(x,y), cvPoint(W+x, H+y), CV_RGB( 100, 0, 0 ), 2, 8, 0 );
 		}
 		if(ith_blob->getlabel() == PERSON){
-			cvRectangle( blobImage, cvPoint(x,y), cvPoint(W+x, H+x), CV_RGB( 0, 0, 100 ), 2, 8, 0 );
+			cvRectangle( blobImage, cvPoint(x,y), cvPoint(W+x, H+y), CV_RGB( 0, 0, 100 ), 2, 8, 0 );
 		}
 		
-		cvRectangle( blobImage, cvPoint(x,y), cvPoint(W+x, H+x), CV_RGB( 0, 255, 0 ), 2, 8, 0 );
+		cvRectangle( blobImage, cvPoint(x,y), cvPoint(W+x, H+y), CV_RGB( 0, 255, 0 ), 2, 8, 0 );
 	}
 
 	//destroy all resources (if required)
